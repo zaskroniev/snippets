77a0bd2bc42f8a1b086e4f45a378c9153cb761b5
zaskroniev <z.napierala@gmail.com>
2023-03-07 10:19:26 +0100

    modification

--- 01-hello-world.py
+++ 01-hello-world.py
@@ -1,5 +1,16 @@
-first_name = input('Jak masz na imię?')
-age = int(input ('Ile masz lat?'))
-# print(first_name)
-print(f'Hello{first_name}!')
-print(f'Mam{age}lat')   #hshusdj
+first_name = input("Jak masz na imię? ")
+age = int(input("Ile masz lat? "))
+
+#print
+#print(f"Hello {first_name}!")
+#print(f"Mam {age} lat.")
+
+if first_name == "Zuzu":
+    print("Cześć Zuzu! Kopę lat!")
+else:
+    print("Miło mi Cię poznać!")
+
+if age >= 18:
+    print("Jesteś pełnoletni.")
+else:
+    print(f"Musisz poczekać jeszcze {18 - age} lat.")
---
+++ 02-calculations.py
@@ -0,0 +1,11 @@
+a = float(input("Podaj pierwszą liczbę: "))
+b = float(input("Podaj drugą liczbę: "))
+
+print("Suma:", a + b)
+print("Różnica:", a - b)
+print("Iloczyn:", a * b)
+print("Iloraz:", a / b)
+print("Reszta z dzielenia (modulo):", a % b)
+print("Całkowita część z dzielenia:", a // b)
+print("a do potęgi b:", a ** b)
+
